8015bfd053fd07445ae53dcb02980885fe64ed83
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-02-23 17:21:16 +0100

    fixed the symbol name

--- src/strchr.asm
+++ src/strchr.asm
@@ -1,10 +1,10 @@
 	BITS	64
-	global	my_strchr:function
+	global	strchr:function
         ;;;The  strchr() function returns a pointer to the first occurrence of the
        ;;;character c in the string s.
 	;;;char *strchr(const char *s, int c);
 	section .text
-my_strchr:
+strchr:
 
 	xor	rax, rax
     mov rax, rdi ; I just return the str arg back for now))
