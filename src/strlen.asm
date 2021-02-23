71e66555e64d6e5e1ec02da2be63817d44b751a2
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-02-23 16:31:23 +0100

    strlen example

--- src/strlen.asm
+++ src/strlen.asm
@@ -1,7 +1,21 @@
 	BITS	64		 ;  64bits asm
-	global	my_strlen:function ; signature int strlen(const char *)
+	global	strlen:function ; signature int strlen(const char *)
 	section	.text		 ;  .text section contains the programm itself
 
-my_strlen:
-    mov rax, 16
+strlen:
+    ;push rbp
+    ;mov rbp, rsp
+    xor rcx, rcx
+
+loop:
+    cmp byte[rdi + rcx], 0x00
+    je end
+    inc rcx
+    jmp loop
+
+end:
+    mov rax, rcx
+
+    ;mov rsp, rbp ; epilog
+    ;pop rbp; epilog ... what WAS in this rbp?? ah ... my string
     ret
