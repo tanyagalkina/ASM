5c30b49ad29e795a466abadc34b02fb4cc11ac42
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-02-23 21:45:26 +0100

    learned enter 0, 0 & leave

--- src/strchr.asm
+++ src/strchr.asm
@@ -4,9 +4,36 @@
        ;;;character c in the string s.
 	;;;char *strchr(const char *s, int c);
 	section .text
+
 strchr:
 
-	xor	rax, rax
-    mov rax, rdi ; I just return the str arg back for now))
+    ;push	rbp			; Add a element to the stack
+    ;mov	rbp,	rsp
+    enter   0,0 ; just trying to replace the prev idiom of creating a stack frame
+	xor	rcx, rcx
+	cmp rdi, 0 ; check if str is NULL
+	je  quit
+
+loop:
+    cmp [rdi + rcx], sil
+    je  quit
+    cmp byte[rdi + rcx], 0
+    je  quit_null ;if the char is not found
+    inc rcx
+    jmp loop
+
+quit_null:
+    mov rax, 0
+    leave
+    ret
+
+quit:
+	add rdi, rcx
+    mov rax, rdi ; I ;just return the str arg back for now))
+
+    ;mov rsp, rbp
+    ;pop rbp
+    leave ; instead of the above stack frame idiom
+
     ret
 	
