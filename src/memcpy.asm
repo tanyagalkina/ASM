bb6af3b0e974d4c7c609c791fd99527b1e1277aa
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-02-24 05:28:31 +0100

    + addressing modes!

--- src/memcpy.asm
+++ src/memcpy.asm
@@ -1,6 +1,6 @@
     BITS    64
     global  memcpy:function
-    ;
+    ;*************************************************************
     ;void *memcpy(void *dest, const void *src, size_t n);
     ;The  memcpy()  function  copies  n bytes from memory area src to memory
     ;area dest.  The memory areas must not overlap.  Use memmove(3)  if  the
@@ -11,5 +11,28 @@
     section .text
 
 memcpy:
-        mov rax, rdi
-        ret
+    enter 0, 0  ;
+    xor rcx, rcx
+
+    cmp rdi, 0 ; rdi stores 1st arg, DEST
+    je  quit
+    cmp rsi, 0 ; rsi stores 2nd arg, SOURCE
+    je  quit
+    cmp rdx, 0; size_t n
+    je  quit; should i check if rdx value is < then 0? the type is unsigned...
+
+loop:
+    cmp byte[rsi + rcx], 0
+    je quit
+    cmp rcx, rdx
+    je quit
+    ;mov [rdi + rcx], [rsi + rcx] why it does not go? addressing modes?
+    mov r9b, [rsi + rcx] ; copy to tpm
+    mov [rdi + rcx], r9b ; copy to DEST[rcx]
+    inc rcx
+    jmp loop
+
+quit:
+    mov rax, rdi
+    leave
+    ret
