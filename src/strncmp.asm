74bab62d47b7e930723e8d1659f6301a1ae170d9
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-02-26 03:30:52 +0100

    strncmp))

--- src/strncmp.asm
+++ src/strncmp.asm
@@ -8,9 +8,37 @@
 
     section .text
 
-strncmp:
 
+
+strncmp:
+    enter 0, 0 ; I dont really need to use the stack cause I dont use any local vars
+    xor rcx, rcx
     xor rax, rax
-    inc rax
-    dec rax
+    cmp rdx, 0
+    je away
+    dec rdx
+    jmp loop
+
+returnc1c2:
+    mov r13b, r9b
+    sub r13b, r8b
+    movsx rax, r13b ; sx suffix moves smaller regs to bigger
+    jmp away
+
+loop:
+    mov	r9b, byte[rdi + rcx]	; c1 = (unsigned char) *s1++;
+	mov	r8b, byte[rsi + rcx]	; c2 = (unsigned char) *s2++;
+	cmp	r9b, 0 		            ; if (c1 == '\0')
+	je	returnc1c2
+    cmp rcx, rdx
+    je  returnc1c2
+	cmp	r9b, r8b	   	; while (c1 == c2);
+	jne	returnc1c2
+	inc	rcx			    ; i++
+	jmp	loop
+
+away:
+    leave
     ret
+
+
