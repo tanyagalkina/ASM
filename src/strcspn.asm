03af9df9c4a18bc7a5945b271fa5e1539d91d634
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-02-26 12:05:59 +0100

    merci!

--- src/strcspn.asm
+++ src/strcspn.asm
@@ -1,5 +1,7 @@
     BITS    64
     global strcspn:function
+    extern rindex
+    extern strlen
     ;*************************************************
     ;size_t strcspn(const char *s, const char *reject);
     ;The strcspn() function calculates the length of the initial segment  of  s
@@ -12,5 +14,33 @@
     section .text
 
 strcspn:
-    mov rax, 1
+    enter 0, 0  ; why =? give some space maybe?
+    xor rcx, rcx ; reset counter
+    xor rax, rax
+    xor r12, r12
+    jmp get_length_second
+
+is_in:
+        cmp byte[rsi + r12], 0 ; the end of reject == end of the loop
+        je get_length_second
+        cmp bl, byte[rsi + r12]
+        je away
+        inc r12
+        jmp is_in
+
+
+get_length_second:
+    xor r12, r12
+    cmp byte[rdi + rcx], 0 ; while (s[i] != '\0') ;
+    ;mov al, byte[r12 + 1]
+    je away
+    mov bl,  byte[rdi + rcx];nto -> tpm
+    inc rcx
+    jmp is_in
+
+away:
+    mov rax, rcx
+    ;mov rax, r12
+   	leave
     ret
+
