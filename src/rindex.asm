f0fa5aca47d005c031f181d301f756d0f82e6a5a
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-02-26 05:11:16 +0100

    wrt!

--- src/rindex.asm
+++ src/rindex.asm
@@ -1,5 +1,6 @@
     BITS    64
     global rindex:function
+    extern strlen
     ;
     ;char *rindex(const char *s, int c);
     ;The rindex() function returns a pointer to the last occurrence  of  the
@@ -14,6 +15,34 @@
     section .text
 
 rindex:
+    enter 0, 0
+    xor rcx, rcx  ; set counter
+    xor rax, rax  ; set return to zero
+    cmp sil, 0
+    jb return_null
 
+call_strlen:
+    push rdi
+    call strlen wrt ..plt   ; PLT: procedure linkage table
+    pop rdi
+    mov rcx,  rax ; put the return value to the counter
+    mov rax, 0
+
+loop:
+    cmp [rdi + rcx], sil
+    je return_value
+    cmp rcx, 0 ; came to the begining and did not found
+    je return_null
+    dec rcx
+    jmp loop
+
+return_value:
     mov rax, rdi
+    add rax, rcx   ; return arg1[counter]
+    leave
+    ret
+
+return_null:
+    mov rax, 0
+    leave
     ret
